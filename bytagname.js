528de1983e635b9f561863fbb5b8a121f5ff604d
Brendan Gasparin <brendan.gasparin@gmail.com>
2021-05-09 08:57:09 +1000

    Implement byTagName function

--- bytagname.js
+++ bytagname.js
@@ -1,7 +1,16 @@
+/*
+    bytagname.js
+    Date: 2021-05-09
+    A function that takes a node and a string, then returns an array of all the node's descendants that have a tag matching the string.
+    This is an exercise from https://eloquentjavascript.net/14_dom.html.
+*/
+
+// return all child elements of node that have the given tag name
 function byTagName(node, tagName) {
-    // Your code here.
+    return node.getElementsByTagName(tagName);
   }
 
+// test byTagName() function
 function setup() {
     console.log(byTagName(document.body, "h1").length);
     // → 1
